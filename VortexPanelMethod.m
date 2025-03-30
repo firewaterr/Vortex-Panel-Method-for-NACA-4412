16896c5d7e79371fba271d22c5b3ba34f922a66a
firewaterr <2200011013@stu.pku.edu.cn>
2025-03-30 23:05:03 +0800

    version 4.0

--- VortexPanelMethod.m
+++ VortexPanelMethod.m
@@ -17,16 +17,14 @@ panels(:,2) = (y_airfoil(1:end-1)+y_airfoil(2:end)) ./ 2; % Control points
 panels(:,3) = sqrt((x_airfoil(1:end-1)-x_airfoil(2:end)).^2 + (y_airfoil(1:end-1)-y_airfoil(2:end)).^2); % Panel lengths
 panels(:,4) = atan((y_airfoil(1:end-1)-y_airfoil(2:end))./(x_airfoil(1:end-1)-x_airfoil(2:end))); % Panel angles
 
-J = zeros(N, N); % Jacobian matrix
 x_con = panels(:,1)'; % Control point x-coordinates
 y_con = panels(:,2)'; % Control point y-coordinates
 
 J = (1 + ((panels(:,2)-y_con)./(panels(:,1)-x_con)).^2).^(-1) .* ...
     (sin(panels(:,4)).*(panels(:,2)-y_con)./(panels(:,1)-x_con).^2 + ...
     cos(panels(:,4)).*(panels(:,1)-x_con).^(-1)); % Directional derivatives
-A = zeros(N, N); % Coefficient matrix for the system of equations
 A = J .* (panels(:,3))'; % Coefficient matrix for the system of equations
-A(isnan(A)) = 0; 
+A(isnan(A)) = 0;
 
 %% Free stream velocity
 V_inf = 10;
@@ -35,19 +33,15 @@ alpha_rad = deg2rad(alpha);
 
 b = V_inf .* cos(pi/2 - (panels(:,4) - alpha_rad)) .* (2 * pi); % Free stream velocity vector
 
-b(N,:) = 0; % Last panel velocity is zero
-A(N,:) = zeros(1,N);
-A(N,round(N/2)-1) = 1;
-A(N,round(N/2)) = 1; % Last panel is a boundary condition
+b(round(N/2),:) = 0; % Last panel velocity is zero
+A(round(N/2),:) = zeros(1,N);
+A(round(N/2),round(N/2):round(N/2)+1) = [1,1];
 
 gamma = A \ b; % Solve for circulation strengths
 
 Gamma = zeros(N,1);
-for i = 1:N-1
-    Gamma(i) = (gamma(i) + gamma(i+1)) / 2; 
-end
-Gamma(N) = gamma(N); % Last panel vortex strength
-
+Gamma(1:end-1) = (gamma(1:end-1)+gamma(2:end))./2;
+Gamma(end) = (gamma(end)+gamma(1))/2;
 %% Calculate velocity diistribution
 V = -1 .* Gamma;
 
